0dcdb62dfc50da6eb52e6e807810390f4be9efa6
enaburekhan <ericenaburekhan2018@gmail.com>
2020-11-03 20:27:56 +0100

    update bot welcome message in bot.rb

--- lib/bot.rb
+++ lib/bot.rb
@@ -11,12 +11,9 @@ class Bot
          case message.text
          when '/start' 
            bot.api.send_message(chat_id: message.chat.id, text: "Hello, #{message.from.first_name}, 
-               welcome to inspirational chat bot created by Eric Enaburekhan, the chat bot is to keep you 
-               inspired and entertained.
-               Use /start to start the bot, 
-               /stop to end the bot,
-               /inspire to get a diffrent inspirational quote everytime you request for it or 
-               /joke to get a joke everytime you request for it")
+            welcome to inspirational chat bot created by Eric Enaburekhan, the chat bot is to keep you 
+            inspired and entertained.Use /start to start the bot, /stop to end the bot,
+            /inspire to get a diffrent inspirational quote everytime you request for it or /joke to get a joke everytime you request for it")
     
            when '/stop'
              bot.api.send_message(chat_id: message.chat.id, text: "Bye, #{message.from.first_name}", date: message.date)  
@@ -34,7 +31,7 @@ class Bot
       
              else
       
-               bot.api.send_message(chat_id: message.chat.id, text: "Invalid entry, #{message.from.first_name}, you need to use  /start,  /stop , /motivate or /joke")
+               bot.api.send_message(chat_id: message.chat.id, text: "Invalid entry, #{message.from.first_name}, you need to use  /start,  /stop , /inspire   or /joke")
           end      
       end      
     end      
--- lib/inspire.rb
+++ lib/inspire.rb
@@ -13,7 +13,6 @@ class Inspire
 
   def make_the_request
     url = 'https://type.fit/api/quotes'
-    # url = 'https://rapidapi.com/api/HealThruWords'
     uri = URI(url)
     response = Net::HTTP.get(uri)
     response = JSON.parse(response)
