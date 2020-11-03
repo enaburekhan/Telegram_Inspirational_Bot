73a2f15dbc6be60c3fdd3df2e278daad9a4ea38a
enaburekhan <ericenaburekhan2018@gmail.com>
2020-11-03 14:09:49 +0100

    add url to yomomma api

--- bin/main.rb
+++ bin/main.rb
@@ -1,13 +1,13 @@
 require_relative '../lib/bot.rb'
 require_relative '../lib/inspire.rb'
 
-puts 'Hello friend! , Welcome to your inspirational telegram bot'
-puts '----------------------------------------------'
-puts 'This bot will  help you stay inspired by messaging you with inspirational quotes'
-puts 'The bot can also keep you entertained by providing you with jokes'
-puts 'This bolt will help keep you moltivated if you are currently demotivated'
-sleep(0.6)
-puts '----------------------------------------------'
-puts '...loading the bot is now online... use ctrl-c to stop the bot'
+# puts 'Hello friend! , Welcome to your inspirational telegram bot'
+# puts '----------------------------------------------'
+# puts 'This bot will  help you stay inspired by messaging you with inspirational quotes'
+# puts 'The bot can also keep you entertained by providing you with jokes'
+# puts 'This bolt will help keep you moltivated if you are currently demotivated'
+# sleep(0.6)
+# puts '----------------------------------------------'
+# puts '...loading the bot is now online... use ctrl-c to stop the bot'
 
 Bot.new
