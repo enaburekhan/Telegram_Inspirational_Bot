591e5e2f69f17604fe2e5bce9fed908e94a7f3d1
enaburekhan <ericenaburekhan2018@gmail.com>
2020-11-03 16:50:35 +0100

    create test for Inspire class

--- lib/bot.rb
+++ lib/bot.rb
@@ -6,37 +6,37 @@ class Bot
   def initialize
     token = '1478214366:AAESsJ-ne12dZG3HY4CO5haNipo5N5r5ZTA'
  
-   Telegram::Bot::Client.run(token) do |bot|
-     bot.listen do |message|
-       case message.text
-       when '/start' 
-         bot.api.send_message(chat_id: message.chat.id, text: "Hello, #{message.from.first_name}, 
-             welcome to inspirational chat bot created by Eric Enaburekhan, the chat bot is to keep you 
-             inspired and entertained.
-             Use /start to start the bot, 
-             /stop to end the bot,
-             /inspire to get a diffrent inspirational quote everytime you request for it or 
-             /joke to get a joke everytime you request for it")
-   
-         when '/stop'
-           bot.api.send_message(chat_id: message.chat.id, text: "Bye, #{message.from.first_name}", date: message.date)  
-           
-         when '/inspire'
-             values = Inspire.new
-             value = values.select_random
-             bot.api.send_message(chat_id: message.chat.id, text: "#{value['text']}", date: message.date)
-     
-           when '/joke'
-             values = Joke.new
-             value = values.make_the_request
-     
-             bot.api.send_message(chat_id: message.chat.id, text: "#{value['joke']}", date: message.date)
-     
-           else
-     
-             bot.api.send_message(chat_id: message.chat.id, text: "Invalid entry, #{message.from.first_name}, you need to use  /start,  /stop , /motivate or /joke")
-         end      
+    Telegram::Bot::Client.run(token) do |bot|
+      bot.listen do |message|
+         case message.text
+         when '/start' 
+           bot.api.send_message(chat_id: message.chat.id, text: "Hello, #{message.from.first_name}, 
+               welcome to inspirational chat bot created by Eric Enaburekhan, the chat bot is to keep you 
+               inspired and entertained.
+               Use /start to start the bot, 
+               /stop to end the bot,
+               /inspire to get a diffrent inspirational quote everytime you request for it or 
+               /joke to get a joke everytime you request for it")
+    
+           when '/stop'
+             bot.api.send_message(chat_id: message.chat.id, text: "Bye, #{message.from.first_name}", date: message.date)  
+             
+           when '/inspire'
+               values = Inspire.new
+               value = values.select_random
+               bot.api.send_message(chat_id: message.chat.id, text: "#{value['text']}", date: message.date)
+      
+             when '/joke'
+               values = Joke.new
+               value = values.make_the_request
+      
+               bot.api.send_message(chat_id: message.chat.id, text: "#{value['joke']}", date: message.date)
+      
+             else
+      
+               bot.api.send_message(chat_id: message.chat.id, text: "Invalid entry, #{message.from.first_name}, you need to use  /start,  /stop , /motivate or /joke")
+          end      
       end      
-   end      
- end
+    end      
+  end
 end
---
+++ spec/telegram_bot_spec.rb
@@ -0,0 +1,36 @@
+require_relative '../lib/inspire.rb'
+require_relative '../lib/joke.rb'
+
+Rspec.describe Inspire do
+  describe '#make_the_request' do
+    let(:request) { Inspire.new } 
+    let(:request_json) { request.make_the_request }
+
+    it 'should return json response if the request is successful' do
+      expect(request_json.class).to eql(Array)    
+    end 
+    
+    it 'returned array response should not be empty' do
+      expect(request_json.length).not_to eql(0)    
+    end    
+  end 
+  
+  describe '#select_random' do
+    let(:random) { Inspire.new }
+    let(:request_random) { random.select_random }
+
+    it 'should return an Hash' do
+      expect(request_random.class).to eql(Hash)    
+    end    
+
+    it 'should return a key and a value' do
+      expect(request_random.length).to eql(2)    
+    end    
+  end  
+end 
+
+
+Rspec.describe Joke do
+  describe '#make_the_request' do
+  end      
+end    
