e13c611a2c2177dd60696f1dcafb721109a02fbc
enaburekhan <ericenaburekhan2018@gmail.com>
2020-11-03 13:37:25 +0100

    add more message to main.rb

--- bin/main.rb
+++ bin/main.rb
@@ -3,9 +3,9 @@ require_relative '../lib/inspire.rb'
 
 puts 'Hello friend! , Welcome to your inspirational telegram bot'
 puts '----------------------------------------------'
-puts 'This is a bot to help you stay motivated by giving you motivational quotes'
+puts 'This bot will  help you stay inspired by messaging you with inspirational quotes'
 puts 'The bot can also keep you entertained by providing you with jokes'
-puts 'This bolt will help you when you feeling down and demotivated'
+puts 'This bolt will help keep you moltivated if you are currently demotivated'
 sleep(0.6)
 puts '----------------------------------------------'
 puts '...loading the bot is now online... use ctrl-c to stop the bot'
